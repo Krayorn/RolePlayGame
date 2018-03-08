0e4c06ac9ac4f5dccafdbc4ab85d67352175dc70
Nathael Arki <nathael.arki@supinternet.fr>
2018-03-08 22:24:28 +0100

    added routes '/chapters' to get all chapters; added folder routes to separate code

--- server/app.js
+++ server/app.js
@@ -1,26 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const Chapter = require('./models/chapter')
+const chapters = require('./routes/chapters')
 
 const app = express()
 const router = express.Router()
+
+const routers = [
+  chapters
+]
 
 mongoose.connect('mongodb://localhost/rolePlayWeb')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-//To prevent errors from Cross Origin Resource Sharing, we will set
-//our headers to allow CORS with middleware like so:
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE')
   res.setHeader('Access-Control-Allow-Headers',
     'Access-Control-Allow-Headers,Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers')
-
-  //and remove cacheing so we get the most recent comments
   res.setHeader('Cache-Control', 'no-cache')
   next()
 })
@@ -29,28 +29,6 @@ router.get('/', function(req, res) {
   res.json({ message: 'API Initialized!'})
 })
 
-router.route('/chapters/:number')
-
-  .get((req, res) => {
-    Chapter.findOne({number: req.params.number}, (err, data) => {
-      if (err) res.send(err)
-      return res.send(data)
-    })
-  })
-
-router.route('/add/chapter')
-
-  .post(function(req, res) {
-    const chapter = new Chapter()
-
-    chapter.title = req.body.title
-    chapter.content = req.body.content
-    chapter.save(function(err) {
-      if (err) res.send(err)
-      res.json({ message: 'Chapter successfully added' })
-    })
-  })
-
-app.use('/', router)
+app.use('/', ...routers)
 
 module.exports = app
---
+++ server/routes/chapters.js
@@ -0,0 +1,36 @@
+const router = require('express').Router()
+
+const Chapter = require('../models/chapter')
+
+router.route('/chapters')
+  .get((req, res) => {
+    Chapter.find({}, (err, data) => {
+      if (err) res.send(err)
+      console.log('data', data)
+      return res.send(data)
+    })
+  })
+
+router.route('/chapters/:number')
+
+  .get((req, res) => {
+    Chapter.findOne({number: req.params.number}, (err, data) => {
+      if (err) res.send(err)
+      return res.send(data)
+    })
+  })
+
+router.route('/add/chapter')
+
+  .post(function(req, res) {
+    const chapter = new Chapter()
+
+    chapter.title = req.body.title
+    chapter.content = req.body.content
+    chapter.save(function(err) {
+      if (err) res.send(err)
+      res.json({ message: 'Chapter successfully added' })
+    })
+  })
+
+module.exports = router
